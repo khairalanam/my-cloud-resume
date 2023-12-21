17b2867a8718dd803b167cd863741685b41e8072
khairalanam <anaambasheer@gmail.com>
2023-12-21 20:10:20 +0530

    Refactor and remove unecessary code

--- scribbler.js
+++ scribbler.js
@@ -1,29 +1,24 @@
-var get = (selector, scope) => {
-  scope = scope ? scope : document;
-  return scope.querySelector(selector);
-};
+// utils
+const get = (selector, scope = document) => scope.querySelector(selector);
+const getAll = (selector, scope = document) => scope.querySelectorAll(selector);
 
-var getAll = (selector, scope) => {
-  scope = scope ? scope : document;
-  return scope.querySelectorAll(selector);
-};
-
+// typewriter animation
 if (document.getElementsByClassName("demo").length > 0) {
-  var i = 0;
-  var speed = 25;
-  var txt = `khairalanam
+  let i = 0;
+  let speed = 25;
+  let txt = `khairalanam
             [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
 
             ### about me
 
-            - i am a web developer and a uiux designer
+            - i am a creative web developer and a designer
             - i like exploring many things in tech and sharing my learnings with the world!
             - email me at: anaambasheer@gmail.com
             - i like tea ☕`;
 
   function typeItOut() {
     if (i < txt.length) {
-      document.getElementsByClassName("demo")[0].innerHTML += txt.charAt(i);
+      document.getElementsByClassName("demo")[0].innerHTML += txt[i];
       i++;
       setTimeout(typeItOut, speed);
     }
@@ -32,78 +27,30 @@ if (document.getElementsByClassName("demo").length > 0) {
   setTimeout(typeItOut, 1000);
 }
 
-window.addEventListener("load", function () {
-  var tabContainers = getAll(".tab__container");
+// tabs section
+window.addEventListener("load", () => {
+  let tabContainers = getAll(".tab__container");
 
-  for (var i = 0; i < tabContainers.length; i++)
+  for (let i = 0; i < tabContainers.length; i++)
     get(".tab__menu", tabContainers[i]).addEventListener("click", tabClick);
 
   function tabClick(event) {
-    var scope = event.currentTarget.parentNode;
-    var clickedTab = event.target;
-    var tabs = getAll(".tab", scope);
-    var panes = getAll(".tab__pane", scope);
-    var activePane = get(`.${clickedTab.getAttribute("data-tab")}`, scope);
+    const scope = event.currentTarget.parentNode;
+    let clickedTab = event.target;
+    let tabs = getAll(".tab", scope);
+    let panes = getAll(".tab__pane", scope);
+    let activePane = get(`.${clickedTab.getAttribute("data-tab")}`, scope);
 
-    for (var i = 0; i < tabs.length; i++) tabs[i].classList.remove("active");
+    for (let i = 0; i < tabs.length; i++) tabs[i].classList.remove("active");
 
-    for (var i = 0; i < panes.length; i++) panes[i].classList.remove("active");
+    for (let i = 0; i < panes.length; i++) panes[i].classList.remove("active");
 
     clickedTab.classList.add("active");
     activePane.classList.add("active");
   }
 });
 
-var btns = getAll(".js-btn");
-var sections = getAll(".js-section");
-
-function setActiveLink(event) {
-  for (var i = 0; i < btns.length; i++) btns[i].classList.remove("selected");
-
-  event.target.classList.add("selected");
-}
-
-function smoothScrollTo(i, event) {
-  var element = sections[i];
-  setActiveLink(event);
-
-  window.scrollTo({
-    behavior: "smooth",
-    top: element.offsetTop - 20,
-    left: 0,
-  });
-}
-
-if (btns.length && sections.length > 0) {
-  for (var i = 0; i < btns.length; i++)
-    btns[i].addEventListener("click", smoothScrollTo.bind(this, i));
-}
-
-window.addEventListener("scroll", () => {
-  var docNav = get(".doc__nav > ul");
-
-  if (docNav) {
-    if (window.scrollY > 63) docNav.classList.add("fixed");
-    else docNav.classList.remove("fixed");
-  }
-});
-
-var topNav = get(".menu");
-var icon = get(".toggle");
-
-window.addEventListener("load", () => {
-  function showNav() {
-    if (topNav.className === "menu") {
-      topNav.className += " responsive";
-      icon.className += " open";
-    } else {
-      topNav.className = "menu";
-      icon.classList.remove("open");
-    }
-  }
-  icon.addEventListener("click", showNav);
-});
-
+// views counter
 const counter = document.querySelector(".viewer__count");
 
 async function updateCounter() {
